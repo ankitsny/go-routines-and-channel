9e897a7066c924031c8524e75020c6a5354f592a
Ankit Kumar <ankit_sony3@hotmail.com>
2018-01-14 10:47:59 +0530

    reading msg from chan

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main(){
+	links := []string{
+		"https://google.com",
+		"https://flipkart.com",
+		"https://amazon.com",
+		"https://stackoverflow.com",
+		"https://netflix.com",
+	}
+	c := make(chan string)
+	for _, link := range links{
+		go checkLink(link, c)
+	}
+
+	// Reading a  message from channel is blocking operation
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
+
+func checkLink(link string, c chan string){
+	if _, err := http.Get(link); err != nil{
+		// fmt.Println(link, "is down")
+		c <- fmt.Sprintf("%s is down", link)
+		return
+	}
+	// fmt.Println(link, "is up")
+	c <- fmt.Sprintf("%s is up", link)
+}
